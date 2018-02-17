7f9daa0c5e0b8109ecadd140de6c1115dfd3b539
Rick Giuly <rgiuly@gmail.com>
2018-02-17 22:21:04 +0000

    commit

--- controller_ws_python36.py
+++ controller_ws_python36.py
@@ -5,15 +5,20 @@ import time
 import argparse
 import rsbot_python36 as rsbot
 import json
+import robot_util_python36 as robot_util
 
 
+
+apiHost = "robotstreamer.com:6001"
 
 
 parser = argparse.ArgumentParser(description='start robot control program')
-
-
+parser.add_argument('robot_id', help='Robot ID')
 parser.add_argument('--forward', default='[-1,1,-1,1]')
 parser.add_argument('--left', default='[1,1,1,1]')
+parser.add_argument('--no-secure-cert', dest='secure_cert', action='store_false')
+
+
 
 
 commandArgs = parser.parse_args()
@@ -25,11 +30,18 @@ rsbot.init(json.loads(commandArgs.forward),
 
 
 
+def getControlHostPort():
 
+        url = 'http://%s/get_control_host_port/%s' % (apiHost, commandArgs.robot_id)
+        response = robot_util.getWithRetry(url, secure=commandArgs.secure_cert)
+        return json.loads(response)
+            
 
 async def hello():
 
-    url = 'ws://54.219.37.103:9310/echo'
+    port = getControlHostPort()['port']
+    print("connecting to port:", port)
+    url = 'ws://54.219.37.103:%s/echo' % port
 
     async with websockets.connect(url) as websocket:
 
