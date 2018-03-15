feef043cd45cbdf8ff59f10fe0e317109cc1063a
Rick Giuly <rgiuly@gmail.com>
2018-03-15 22:18:35 +0000

    commit

--- rsbot_python36.py
+++ rsbot_python36.py
@@ -205,7 +205,7 @@ def move(command):
                         time.sleep(2.8)
                         print("free ping pong", freePongActive)
                         pingPongMotor.run(Adafruit_MotorHAT.RELEASE)
-                        time.sleep(40)
+                        time.sleep(60)
                         freePongActive = False
                     else:
                         print("ping pong not enabled")
