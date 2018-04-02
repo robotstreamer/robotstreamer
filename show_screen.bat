f8c7deeee6c5d4c9b856a5927dc8c7eef3fe6834
Rick Giuly <rgiuly@gmail.com>
2018-04-02 13:12:03 -0700

    commit

--- show_screen.bat
+++ show_screen.bat
@@ -1,6 +1,6 @@
 
 :loop
-python robotstreamer/old_send_video.py YOURCAMERAID 0 --screen-capture --kbps 2500 --audio-input-device "Microphone (HD Webcam C270)"
+py -2 send_video_windows.py YOURCAMERAID 0 --screen-capture --kbps 2500 --audio-input-device "Microphone (HD Webcam C270)"
 timeout 2
 goto loop
 
