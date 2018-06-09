57696044d5f1ab2f95ead50e6b9b91bae00d1555
Rick Giuly <rgiuly@gmail.com>
2018-06-09 06:04:14 -0700

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -73,27 +73,29 @@ sudo apt-get -y install emacs &&\
 #sudo apt-get -y install python-dev &&\
 sudo python3 setup.py install &&\
 
-cd ~ &&\
-wget ftp://ftp.alsa-project.org/pub/lib/alsa-lib-1.0.25.tar.bz2 &&\
-tar xjf alsa-lib-1.0.25.tar.bz2 &&\
-cd alsa-lib-1.0.25 &&\
-./configure --host=arm-unknown-linux-gnueabi &&\
-make -j4 &&\
-sudo make install &&\
+#cd ~ &&\
+#wget ftp://ftp.alsa-project.org/pub/lib/alsa-lib-1.0.25.tar.bz2 &&\
+#tar xjf alsa-lib-1.0.25.tar.bz2 &&\
+#cd alsa-lib-1.0.25 &&\
+#./configure --host=arm-unknown-linux-gnueabi &&\
+#make -j4 &&\
+#sudo make install &&\
 
-cd ~ &&\
-git clone git://git.videolan.org/x264 &&\
-cd x264 &&\
-./configure --host=arm-unknown-linux-gnueabi --enable-static --disable-opencl &&\
-make -j4 &&\
-sudo make install &&\
+#cd ~ &&\
+#git clone git://git.videolan.org/x264 &&\
+#cd x264 &&\
+#./configure --host=arm-unknown-linux-gnueabi --enable-static --disable-opencl &&\
+#make -j4 &&\
+#sudo make install &&\
 
-cd ~ &&\
-git clone https://github.com/FFmpeg/FFmpeg.git &&\
-cd FFmpeg &&\
-./configure --arch=armel --target-os=linux --enable-gpl --enable-libx264 --enable-nonfree --extra-libs=-ldl &&\
-make -j4 &&\
-sudo make install
+#cd ~ &&\
+#git clone https://github.com/FFmpeg/FFmpeg.git &&\
+#cd FFmpeg &&\
+#./configure --arch=armel --target-os=linux --enable-gpl --enable-libx264 --enable-nonfree --extra-libs=-ldl &&\
+#make -j4 &&\
+#sudo make install
+
+sudo apt-get install ffmpeg
 
 cd ~ &&\
 git clone https://github.com/robotstreamer/robotstreamer &&\
