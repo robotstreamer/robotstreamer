4b3ba06acfce4f3d5ce8c15794fb84321a95b9b3
Rick Giuly <rgiuly@gmail.com>
2020-06-17 17:14:54 -0700

    adding example http requests

--- http_example_interface.py
+++ http_example_interface.py
@@ -1,25 +1,29 @@
 
 import requests 
+from time import sleep
   
 for i in range(10):
 
   print(i)
 
-  URL = "http://192.168.178.28/?value=50"
+  URL = "http://192.168.178.28/"
+  
+  #URL = "https://postman-echo.com/"
+  
   PARAMS = {'value':50}
   print(URL, PARAMS)
   r = requests.get(url = URL, params = PARAMS) 
-  data = r.json() 
-  print(data)
+  #data = r.json() 
+  print(r.content)
   
   sleep(1)
 
-  URL = "http://192.168.178.28/?value=100"
+  
   PARAMS = {'value':120} 
   print(URL, PARAMS)
   r = requests.get(url = URL, params = PARAMS) 
-  data = r.json() 
-  print(data)
+  #data = r.json() 
+  print(r.content)
 
   sleep(1)
 
