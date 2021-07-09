de584ad7bbe51c6ec478fc0dd342c926088707b7
Rick Giuly <rgiuly@gmail.com>
2021-07-09 01:42:10 -0400

    lovense speed

--- lovense_interface.py
+++ lovense_interface.py
@@ -6,14 +6,18 @@ URL = "http://127-0-0-1.lovense.club:20010/Vibrate"
 def init():
   print("init")
                 
-    
+
+def getSpeed(command):
+  # assume the command is like vibrate50 or vibrate100
+  return int(command[7:])
+
 def handleCommand(command, keyPosition):
 
                 print("***********************************", command)
-                if command == 'F':
+                if command[0:7] == 'vibrate':
                   print('\a')
-                  print("command is F")
-                  PARAMS = {'v':50}
+                  print("command is " + str(command))
+                  PARAMS = {'v':getSpeed(command)}
                   print(URL, PARAMS)
                   r = requests.get(url = URL, params = PARAMS) 
                   print(r)
@@ -25,21 +29,6 @@ def handleCommand(command, keyPosition):
                   print(r)
                   print(r.content)
 
-
-                if command == 'B':
-                  print('\a')
-                  print("command is B")
-                  PARAMS = {'v':100}
-                  print(URL, PARAMS)
-                  r = requests.get(url = URL, params = PARAMS) 
-                  print(r)
-                  print(r.content)
-                  sleep(3)      
-                  PARAMS = {'v':0}
-                  print(URL, PARAMS)
-                  r = requests.get(url = URL, params = PARAMS) 
-                  print(r)
-                  print(r.content)
                 
 
 
